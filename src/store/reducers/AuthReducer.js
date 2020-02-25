062f9316cf8b6ece1e1ab2235aeee3962b46f56f
Thiago Butignon <thiagobutignon@gmail.com>
2020-02-24 22:20:44 -0300

    Implementado o switch para controlar estado de login e profile do usuário

---
+++ src/store/reducers/AuthReducer.js
@@ -0,0 +1,34 @@
+import * as ActionTypes from "../actions/ActionTypes";
+
+export const initialState = {
+  is_authenticated: false,
+  profile: null
+};
+
+export const AuthReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionTypes.LOGIN_SUCCESS:
+      return {
+        ...state,
+        is_authenticated: true
+      };
+    case ActionTypes.LOGIN_FAILURE:
+      return {
+        ...state,
+        is_authenticated: false
+      };
+    case ActionTypes.ADD_PROFILE:
+      return {
+        ...state,
+        profile: action.payload
+      };
+
+    case ActionTypes.REMOVE_PROFILE:
+      return {
+        ...state,
+        profile: null
+      };
+    default:
+      return state;
+  }
+};
